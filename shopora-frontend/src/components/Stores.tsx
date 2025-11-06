cf01ff1a3e2708b833de767099a54ab83f55e8fa
hassanlmezher <hassanmezher084@gmail.com>
2025-11-06 12:10:21 +0200

    adding the function to the back button in the stores page

--- shopora-frontend/src/components/Stores.tsx
+++ shopora-frontend/src/components/Stores.tsx
@@ -3,8 +3,10 @@ import moustache from "../images/moustache.png"
 import gucci from "../images/gucci.png"
 import apple from "../images/apple.png"
 import StoreCard from "./StoreCard";
+import { useNavigate } from 'react-router-dom'
 
 function Stores() {
+    const navigate = useNavigate();
     const stores = [
         {
             image: moustache,
@@ -21,7 +23,7 @@ function Stores() {
     ];
   return (
     <div>
-      <img className="w-30 mt-6 ml-6" src={back} alt="back button" />
+      <img className="w-30 mt-6 ml-6 cursor-pointer" src={back} alt="back button" onClick={() => navigate('/dashboard')} />
       <div className="m-10 pl-10 pr-10 grid grid-cols-3 gap-15 pl-20">
         {stores.map(store => <StoreCard image={store.image} name={store.name} />)}
       </div>
