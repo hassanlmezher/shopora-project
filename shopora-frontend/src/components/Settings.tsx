5939475f045b89c55301da321e46555da5830534
hassanlmezher <hassanmezher084@gmail.com>
2025-11-06 15:00:54 +0200

    Fixing the stylings in the dashboard and creating another dashboard for Logged in users

--- shopora-frontend/src/components/Settings.tsx
+++ shopora-frontend/src/components/Settings.tsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './header'
 
 function Settings() {
   return (
     <div>
+      <Header />
       
     </div>
   )
--- shopora-frontend/src/components/header.tsx
+++ shopora-frontend/src/components/header.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import logo from "../images/Logo.png"
+import lightMode from "../images/lightMode.png"
 
-function header() {
+function Header() {
   return (
-    <div>
-      
+    <div className="bg-white mr-5 rounded-2xl flex justify-between items-center pl-5 pr-10 h-19">
+        <img className="w-30" src={logo} alt="logo" />
+        <img className="w-[40px] h-[40px]" src={lightMode} alt="" />
     </div>
   )
 }
 
-export default header
+export default Header
