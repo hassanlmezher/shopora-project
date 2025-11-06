38c7f1eada54af11f02ab1a14f00e85c7ff07d1c
hassanlmezher <hassanmezher084@gmail.com>
2025-11-06 16:54:35 +0200

    adding a header and the profile pic wioth the username in the settings component

--- shopora-frontend/src/components/Settings.tsx
+++ shopora-frontend/src/components/Settings.tsx
@@ -2,6 +2,10 @@ import Header from "./Header"
 import back from "../images/back.png"
 import { useNavigate } from "react-router-dom";
 import useAuthStore from '../store/useAuthStore';
+import profile from "../images/profile.png";
+import heart from "../images/heart.png";
+import cartt from "../images/cartt.png";
+import create from "../images/create.png"
 
 function Settings() {
   const navigate = useNavigate();
@@ -11,6 +15,15 @@ function Settings() {
       <Header/>
       <div className="pl-15">
         <img className="w-30" src={back} alt="back button" onClick={() => isLoggedIn? navigate('/DashboardLoggedIn') : navigate('/dashboard')}/>
+      </div>
+      <div className="h-130 flex justify-center items-center">
+        <img className="absolute w-35 top-30" src={profile} alt="profile pic" />
+        <p className="font-bold text-2xl absolute top-70 ml-3">Hassan Mezher</p>
+      </div>
+      <div className="border border-black h-70 mt-[-300px] flex justify-center items-center">
+        <div className="bg-">
+
+        </div>
       </div>
     </div>
   )
