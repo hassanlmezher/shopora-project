53f438bf005395bfd43a74a20e51b280062b5f44
hassanlmezher <hassanmezher084@gmail.com>
2025-11-06 12:01:25 +0200

    finshing hardcoded data for the stores page

--- shopora-frontend/src/components/StoreCard.tsx
+++ shopora-frontend/src/components/StoreCard.tsx
@@ -5,10 +5,10 @@ interface StoreItem {
 
 function StoreCard({image, name} : StoreItem) {
   return (
-    <div className="border-1 border-[#939393] flex flex-col justify-center items-center pt-5">
+    <div className="border-1 border-gray-300 w-70 rounded-3xl  flex flex-col justify-center items-center pt-5 pb-10">
         <p className="bg-[#F7F0E0] p-5 px-15 rounded-br-3xl rounded-t-3xl">{name}</p>
         <img className="w-35 mt-5" src={image} alt="store logo" />
-      
+        <button className="bg-[#EAEAEA] font-bold mt-8 w-30 h-10 transition duration-300 ease-in-out hover:bg-black hover:text-[#F7F0E0]">Explore</button>
     </div>
   )
 }
--- shopora-frontend/src/components/Stores.tsx
+++ shopora-frontend/src/components/Stores.tsx
@@ -22,7 +22,7 @@ function Stores() {
   return (
     <div>
       <img className="w-30 mt-6 ml-6" src={back} alt="back button" />
-      <div className="m-10 pl-10 pr-10 grid grid-cols-3 gap-15">
+      <div className="m-10 pl-10 pr-10 grid grid-cols-3 gap-15 pl-20">
         {stores.map(store => <StoreCard image={store.image} name={store.name} />)}
       </div>
     </div>
