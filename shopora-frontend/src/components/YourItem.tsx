622958be6a410c57a6cfee8ae73344ec387e1935
hassanlmezher <hassanmezher084@gmail.com>
2025-11-15 14:26:11 +0200

    initialising the yourItem card cocomponent ad adding the navigate and handle details function

--- shopora-frontend/src/components/YourItem.tsx
+++ shopora-frontend/src/components/YourItem.tsx
@@ -3,3 +3,19 @@ import shirt from ",,/images/shirt.png";
 import starts from "../images/5stars.png";
 import { type UserShopItem } from "../store/useNotificationStore";
 
+interface YourItemProps {
+    item: UserShopItem;
+    onRemove?: () => void;
+}
+
+function YourItem({ item, onRemove }: YourItemProps) {
+    const navigate = useNavigate();
+
+    const hadnleDetails = () => {
+        navigate("/details", { state: { ...item, returnPath: "/my-shop" } });
+    };
+
+    return(
+        
+    );
+}
