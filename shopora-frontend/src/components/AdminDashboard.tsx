5aa54f5e83d0b485e8695878a88ccc91041363ab
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-11-29 14:36:21 +0200

    allowing the admin to ban and unban a shop

--- shopora-frontend/src/components/AdminDashboard.tsx
+++ shopora-frontend/src/components/AdminDashboard.tsx
@@ -76,7 +76,7 @@ function AdminDashboard() {
           <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
             {totalShopCount > 0 ? (
               <>
-                {stores.map((store) => (
+                {stores.filter((store) => !store.banned).map((store) => (
                   <AdminShopCard
                     key={store.id}
                     storeName={store.name}
