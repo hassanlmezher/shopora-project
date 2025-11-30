4b96cf93390c101281f4ea4e73b8c59c43035881
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-11-30 20:31:25 +0200

    adding the controller of the authorization

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
+
 import authRoutes from "./routes/authRoutes.js";
 import shopRoutes from "./routes/shopRoutes.js";
 import itemRoutes from "./routes/itemRoutes.js";
-import favoriteRoutes from "./routes/orderRoutes.js";
+import favoriteRoutes from "./routes/favoriteRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
 import notificationRoutes from "./routes/notificationRoutes.js";
 
@@ -16,11 +17,12 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+// API Routes
 app.use("/api/auth", authRoutes);
-app.use("api/shops", shopRoutes);
+app.use("/api/shops", shopRoutes);
 app.use("/api/items", itemRoutes);
 app.use("/api/favorites", favoriteRoutes);
-app.use("/api/oders", orderRoutes);
+app.use("/api/orders", orderRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 const PORT = process.env.PORT || 5000;
