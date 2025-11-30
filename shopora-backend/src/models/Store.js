5a0cf1a1ad0273ebcb91e6d87c7543a0d243f79a
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-11-30 19:42:52 +0200

    initialing the backend folder with some files

--- shopora-backend/src/models/Store.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const storeSchema = new mongoose.Schema({
-    name: String,
-    image: String,
-    description: String,
-});
-
-export default mongoose.model("Store", storeSchema);
