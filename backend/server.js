97ed7df54c317fd2d3c156c575b539dc2f7175a2
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-05 17:38:07 +0200

    fixing the hidden logs in the server.js file

--- backend/server.js
+++ backend/server.js
@@ -18,16 +18,18 @@ async function connectDB() {
 
         console.log("MongoDB Connected Successfully");
     } catch (error) {
-        console.error("MongoDB Connection Error:", error.message);
+        console.error("Mongo Connection Error:", error.message);
         process.exit(1);
-    } 
-
-    connectDB();
-
-    app.get("/", (req, res) => {
-        res.send("shopora backend running!");
-    });
-
-    const PORT = process.env.PORT || 5000;
-    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+    }
 }
+
+connectDB();
+
+app.get("/", (req, res) => {
+    res.send("shopora backend running!");
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
