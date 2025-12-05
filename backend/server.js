1f2c186e81ca8c12d4d75324703aedc4cc3392b3
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-05 17:23:33 +0200

    importing the necessary stuff for the server.js file

--- backend/server.js
+++ backend/server.js
@@ -2,3 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
+
+dotenv.config();
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
