b4546ae11cf9f57c2e9fe3e33913212c62924b26
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-05 17:26:52 +0200

    connecting to the mongodb database

--- backend/server.js
+++ backend/server.js
@@ -10,3 +10,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+async function connectDB() {
+    try{
+        await mongoose.connect(process.env.MONGO_URI, {
+            dbName: "shopora",
+        });
+
+        console.log("MongoDB Connected Successfully");
+    } catch (error) {
+        console.error("MongoDB Connection Error:", error.message);
+        process.exit(1);
+    } 
+
+    connectDB();
+
+    app.get("/", (req, res) => {
+        res.send("shopora backend running!");
+    });
+
+    const PORT = process.env.PORT || 5000;
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
