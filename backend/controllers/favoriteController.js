f8f569271332335f47295395108a1a32306a2ba8
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-12 12:17:24 +0200

    adding the favorites controller

---
+++ backend/controllers/favoriteController.js
@@ -0,0 +1,10 @@
+import Favorite from "../models/Favorite.js";
+
+export const toggleFavorite = async (req, res) => {
+  const fav = await Favorite.findOneAndUpdate(
+    { user: req.body.user },
+    { $addToSet: { products: req.body.product } },
+    { upsert: true, new: true }
+  );
+  res.json(fav);
+};
