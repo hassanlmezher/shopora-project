f307f077e9ff8b49f81481423b0e2a5a017ae425
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-12 12:20:34 +0200

    adding the order controller

---
+++ backend/controllers/orderController.js
@@ -0,0 +1,11 @@
+import Order from "../models/Order.js";
+
+export const createOrder = async (req, res) => {
+  const order = await Order.create(req.body);
+  res.status(201).json(order);
+};
+
+export const getOrdersByUser = async (req, res) => {
+  const orders = await Order.find({ user: req.params.userId });
+  res.json(orders);
+};
