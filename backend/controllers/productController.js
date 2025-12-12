549079217ea76d6ac1e8cacecb478c52e2f5fbe9
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-12 12:10:36 +0200

    adding the product controller

---
+++ backend/controllers/productController.js
@@ -0,0 +1,21 @@
+import Product from "../models/Product.js";
+
+export const createProduct = async (req, res) => {
+  const product = await Product.create(req.body);
+  res.status(201).json(product);
+};
+
+export const getProducts = async (req, res) => {
+  const products = await Product.find().populate("store");
+  res.json(products);
+};
+
+export const getProductById = async (req, res) => {
+  const product = await Product.findById(req.params.id).populate("store");
+  res.json(product);
+};
+
+export const deleteProduct = async (req, res) => {
+  await Product.findByIdAndDelete(req.params.id);
+  res.json({ message: "Product deleted" });
+};
