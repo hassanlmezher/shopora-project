b63320f7732fa95f9fb40d13ebf4299eda02c6d7
Hassan Mezher <hassanmezher@Hassans-MacBook-Air.local>
2025-12-21 20:19:04 +0200

    removing unecessary files

--- backend/dist/routes/cartRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { addToCart, clearCart, getCart, removeCartItem, updateCartItem, } from "../controllers/cartController.js";
-const router = express.Router();
-router.get("/:userId", getCart);
-router.post("/", addToCart);
-router.patch("/:userId/items/:productId", updateCartItem);
-router.delete("/:userId/items/:productId", removeCartItem);
-router.delete("/:userId", clearCart);
-export default router;
--- backend/dist/routes/favoriteRoutes.js
+++
@@ -1,6 +0,0 @@
-import express from "express";
-import { getFavorites, toggleFavorite } from "../controllers/favoriteController.js";
-const router = express.Router();
-router.post("/", toggleFavorite);
-router.get("/:userId", getFavorites);
-export default router;
